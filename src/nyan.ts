d7bc7edc4cfc57dff141bd5b688cacade98cf668
zhangyunhua <zy.hua1122@qq.com>
2022-08-14 00:05:45 +0800

    fix: format code

--- src/nyan.ts
+++ src/nyan.ts
@@ -67,7 +67,7 @@ export const createNyan = (): (() => { dispose(): void }) => {
       : window.onDidChangeTextEditorSelection;
 
   return () => {
-    updateNyan();
+    handleUpdateNyan();
 
     onDidChange(handleUpdateNyan);
 
